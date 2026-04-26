7e5ba0ae210c5d1a38cea86e7ddbc2cc093578e3
mchoudhu6 <mchoudhu@usc.edu>
2026-04-26 18:55:15 -0500

    updated posts.js

--- posts.js
+++ posts.js
@@ -3,12 +3,12 @@ const posts = [
       title: "Thoughts on Example 1",
       url: "posts/post-01.html",
       date: "2026-04-26",
-      week: "Week 1"
+      week: "Week 2"
     },
     {
       title: "Thoughts on Example 2",
       url: "posts/post-02.html",
       date: "2026-04-20",
-      week: "Week 2"
+      week: "Week 1"
     }
   ];
