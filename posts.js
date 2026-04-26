3171086da16ebd2da751ae99743330528e39882c
mchoudhu6 <mchoudhu@usc.edu>
2026-04-26 18:33:19 -0500

    added posts.js and updated thoughts.html

---
+++ posts.js
@@ -0,0 +1,12 @@
+const posts = [
+    {
+      title: "Thoughts on Example 1",
+      url: "posts/post-01.html",
+      date: "2026-04-26"
+    },
+    {
+      title: "Thoughts on Example 2",
+      url: "posts/post-02.html",
+      date: "2026-04-20"
+    }
+  ];
