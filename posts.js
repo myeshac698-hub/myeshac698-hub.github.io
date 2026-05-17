4c7ebfcf03dfdd4ec3f5f9f1e8ae0e2437ec5fff
Myesha Choudhury <myeshac698@gmail.com>
2026-05-17 18:37:50 -0500

    updated posts.js, style.css, and thoughts.html

--- posts.js
+++ posts.js
@@ -2,13 +2,13 @@ const posts = [
     {
       title: "Thoughts on Example 1",
       url: "posts/post1.html",
-      date: "2026-04-26",
+      date: "2026-04-20",
       week: 1
     },
     {
       title: "Thoughts on Example 2",
       url: "posts/post2.html",
-      date: "2026-04-20",
+      date: "2026-04-26",
       week: 2
     }
   ];
