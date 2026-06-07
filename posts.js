7b5fb271e7ff46b1618e22fb975ba7c63fda0b84
Myesha Choudhury <myeshac698@gmail.com>
2026-06-07 17:17:58 -0500

    added tags

--- posts.js
+++ posts.js
@@ -3,12 +3,14 @@ const posts = [
       title: "Thoughts on Example 1",
       url: "posts/post1.html",
       date: "2026-04-20",
-      week: 1
+      week: 1,
+      tags: ["Book", "Favorite"]
     },
     {
       title: "Thoughts on Example 2",
       url: "posts/post2.html",
       date: "2026-04-26",
-      week: 2
+      week: 2,
+      tags: ["Movie", "Drama"]
     }
   ];
