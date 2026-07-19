80b0f4496f6ebc206499f2096bd78afbe82ec09f
Myesha Choudhury <myeshac698@gmail.com>
2026-07-19 17:36:29 -0500

    added additional fields for posts

--- posts.js
+++ posts.js
@@ -4,6 +4,8 @@ window.posts = [
       url: "posts/post1.html",
       date: "2026-04-20",
       week: 1,
+      mediaType: "Book",
+      rating: 4.5,
       tags: ["Book", "Favorite"],
       excerpt: "A few thoughts on why this book surprised me more than I expected."
     },
@@ -12,6 +14,8 @@ window.posts = [
       url: "posts/post2.html",
       date: "2026-04-26",
       week: 2,
+      mediaType: "Movie",
+      rating: 4.5,
       tags: ["Movie", "Drama"],
       excerpt: "An emotional film that stayed with me long after the credits rolled."
     }
